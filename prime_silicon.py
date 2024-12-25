2a4b34a67a5f2d78041953b61e68dded5d4e974d
Basith Ali KP <sithukp50@gmail.com>
2024-12-25 20:01:52 +0530

    Avoided the use of Boolean variable

--- prime_silicon.py
+++ prime_silicon.py
@@ -1,7 +1,5 @@
 def prime(n):
-    is_prime=True
     for i in range(2,(n//2)+1):
         if n%i==0:
-            is_prime=False
-            break
-    return is_prime
+            return False
+    return True
